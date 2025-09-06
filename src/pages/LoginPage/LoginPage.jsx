ffda9c0a4b93ee3f05d4e79b9ce807c0ff6bbd72
RenatoLetelier <renato.letelier@gmail.com>
2025-09-06 13:01:00 -0400

    env things

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -18,8 +18,6 @@ export default function LoginPage() {
     setError(null);
     setLoading(true);
 
-    console.log(import.meta.env.VITE_API_URL);
-
     if (!email || !password) {
       setError("Please enter both email and password.");
       setLoading(false);
