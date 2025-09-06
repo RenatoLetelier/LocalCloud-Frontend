52d3e33d894ab702cdd3965fdd276fd02b09dc0f
RenatoLetelier <renato.letelier@gmail.com>
2025-09-06 12:08:17 -0400

    fix login with backend

--- src/api/auth.js
+++ src/api/auth.js
@@ -2,4 +2,10 @@ import axios from "axios";
 
 const SUBURL = "/api/auth";
 
-export const loginRequest = (user) => axios.post(`${SUBURL}/login`, user);
+export const loginRequest = (user) =>
+  axios({
+    url: `${SUBURL}/login`,
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    data: user,
+  });
